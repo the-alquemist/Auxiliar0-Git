34bca28f2de545ed05b8e4284528c841fe3d6e93
the-alquemist <jacosta1@hotmail.cl>
2025-03-19 19:09:36 -0300

    persona A 2do push

--- usuario.py
+++ usuario.py
@@ -11,6 +11,6 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+            else: # <<<<<<< Elimina esta
+                print(f"La tarea {tarea.obtenerNombre()} no está lista")
